1045fbc903c90b8914312f2d75d17922bc6e8d28
costa.erick <costa.erick@hcosta.com.br>
2023-11-07 15:39:12 -0300

    Início do projeto

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,30 @@
 import { Header } from './components/headers';
-import { Post } from './post';
+import { Post } from './components/Post';
+import { Sidebar } from './components/sidebar';
 
-import './styles.css'
+import styles from './App.module.css'
+
+import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Erick Garcia Costa"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam repudiandae, quis, sapiente facere fugit aut expedita nemo dicta molestias nesciunt veniam atque dolores? Voluptatem error odio natus perspiciatis ut!"
-      />    
-      <Post 
-      author="Peters Parkers"
-      content="Meu irmão!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Erick Garcia Costa"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium laboriosam repudiandae, quis, sapiente facere fugit aut expedita nemo dicta molestias nesciunt veniam atque dolores? Voluptatem error odio natus perspiciatis ut!"
+          />    
+          <Post 
+            author="Peters Parkers"
+            content="Meu irmão!"
+          />
+        </main>
+      </div>
+
       </div>
   )
 }
